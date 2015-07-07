273ba29c2244c13af9371eeac71583bc389e0722
jinger8910 <jinger8910@gmail.com>
2015-07-07 19:00:50 -0400

    redeploy

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ router.use(function (req,res,next){
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         next();
-	next();
 });
 
 router.route('/path')
